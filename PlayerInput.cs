7a5df81ffa3f9b69d745742ce989bb8ae92e2e21
d0l1ac <43551316+d0l1ac@users.noreply.github.com>
2018-09-25 19:54:07 +0200

    PlayerInput (C#)
    PlayerInput.css script of the Dungeon Crawler project

---
+++ PlayerInput.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class to manage all player keyboard input
+public class PlayerInput : MonoBehaviour {
+
+    private PlayerMove playerMove;		// reference to PlayerMove script
+
+	// Use this for initialization
+	void Awake () 
+    {
+		// init references
+        playerMove = GetComponent<PlayerMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		// if player is not moving (return value of IsMoving is false)
+        if (!playerMove.IsMoving())
+        {
+            GetPlayerInput();		// call function GetPlayerInput from this script
+        }
+	}
+
+	// function to manage player keyboard input
+    private void GetPlayerInput()
+    {
+		// variables of horizontal moving, vertical moving and turning
+        int horizontal = 0;
+        int vertical = 0;
+        int turning = 0;
+
+		// cache movement and turning values based on input axis
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+        turning = (int)(Input.GetAxisRaw("Turning"));
+
+		// limit movement and turning values so that only one value is something than zero
+        if (horizontal != 0)
+        {
+            vertical = 0;
+            turning = 0;
+
+            playerMove.MovePlayer(horizontal, 0, 0);		// call function MovePlayer from this script
+        }
+        else if (vertical != 0)
+        {
+            horizontal = 0;
+            turning = 0;
+
+            playerMove.MovePlayer(0, vertical, 0);			// call function MovePlayer from this script
+        }
+        else if (turning != 0)
+        {
+            horizontal = 0;
+            vertical = 0;
+
+            playerMove.MovePlayer(0, 0, turning);			// call function MovePlayer from this script
+        }
+    }
+}
